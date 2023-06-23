e51f3d3138c7495efa770e9ef3af6ef9b7d5667d
Matheus <matheusdiegoteixeira@gmail.com>
2023-06-23 19:31:03 -0300

    top carousel fix

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ function whatsapp() {
 
     window.open(url, '_blank').focus();
 }
+
+let mybutton = document.getElementById('toTop');
+window.onscroll = function () {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 150 ||
+        document.documentElement.scrollTop > 150
+    ) {
+        document
+            .getElementById('toTop')
+            .classList.remove('visually-hidden');
+    } else {
+        document
+            .getElementById('toTop')
+            .classList.add('visually-hidden');
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
