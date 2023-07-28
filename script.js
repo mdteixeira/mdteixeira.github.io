b6dd12bd8d8732b8a3271f26940d9c08b0500f50
Matheus <matheusdiegoteixeira@gmail.com>
2023-07-28 17:25:23 -0300

    small fixes

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function whatsapp() {
     var nome = document.getElementById('nome').value;
-    var sobrenome = document.getElementById('sobrenome').value;
     var mensagem = document.getElementById('comentario').value;
     var newnome = '';
 
     if (nome.length > 0) {
         newnome = '. Me chamo ' + nome;
-    }
-    if (sobrenome.length > 0) {
-        newnome += ' ' + sobrenome;
     }
     newnome += '.';
     var url =
